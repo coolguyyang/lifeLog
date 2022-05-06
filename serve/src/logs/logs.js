09d069ae099dcbe019c4633f884e0f68954bcaf9
coolguy <1648680588@qq.com>
2022-05-06 22:55:14 +0800

    code logs

--- serve/src/logs/logs.js
+++ serve/src/logs/logs.js
@@ -1,10 +1,8 @@
-const db = require('../../db/db')
-const config = require('../../config')
+const db = require("../../db/db")
+const config = require("../../config")
 
 class Logs {
-  constructor() {
-
-  }
+  constructor() { }
   async add (ctx) {
     let info = ctx.request.body
     // console.log("info:", info)
@@ -12,97 +10,102 @@ class Logs {
     if (!info.uid || !info.ctime || !info.cpos || !info.text) {
       ctx.body = {
         status: 1,
-        uid:info.uid,
-        message: "添加信息不足！"
+        uid: info.uid,
+        message: "添加信息不足！",
       }
       return
     }
     //将信息插入数据库
-    const sqlIns = 'insert into logs set ?'
+    const sqlIns = "insert into logs set ?"
     let res = await new Promise((resolve, reject) => {
-      db.query(sqlIns, {
-        uid: info.uid,
-        ctime: info.ctime,
-        cpos: JSON.stringify(info.cpos),
-        time: info.time,
-        pos: JSON.stringify(info.pos),
-        text: info.text,
-        share: info.share,
-        img: JSON.stringify(info.img),
-        video: JSON.stringify(info.video)
-      }, function (err, results) {
-        if (err) {
-          reject({ status: 1, message: err.message })
-        }
-        else if (results.affectedRows !== 1) {
-          resolve({ status: 1, message: '添加失败，请稍后再试！' })
-        }
-        else {
-          resolve({ status: 0, message: '添加成功' })
-        }
+      db.query(
+        sqlIns,
+        {
+          uid: info.uid,
+          ctime: info.ctime,
+          cpos: JSON.stringify(info.cpos),
+          time: info.time,
+          pos: JSON.stringify(info.pos),
+          text: info.text,
+          share: info.share,
+          img: JSON.stringify(info.img),
+          video: JSON.stringify(info.video),
+        },
+        function (err, results) {
+          if (err) {
+            reject({ status: 1, message: err.message });
+          }
+          else if (results.affectedRows !== 1) {
+            resolve({ status: 1, message: "添加失败，请稍后再试！" });
+          }
+          else {
+            resolve({ status: 0, message: "添加成功" });
+          }
 
-      })
-    })
+        });
+    });
 
-    ctx.body = res
-    console.log(res.message)
+    ctx.body = res;
+    console.log(res.message);
   }
   async findBy (ctx) {
-    let info = ctx.query
-    let condition = ''
-    let valueKey = ''
-    console.log(info)
+    let info = ctx.query;
+    let condition = "";
+    let valueKey = "";
+    console.log(info);
     if (Object.keys(info).length > 0) {
-      condition += 'where '
+      condition += "where ";
       Object.keys(info).forEach((v, i) => {
-        if(!info[v]){
-          valueKey = v
+        if (!info[v]) {
+          valueKey = v;
         }
         if (i !== 0) {
-          condition += ` and `
+          condition += ` and `;
         }
-        condition += `${v}=${info[v]}`
+        condition += `${v}=${info[v]}`;
       })
     }
-    if(valueKey){
+    if (valueKey) {
       ctx.body = {
         status: 1,
-        message: `${valueKey}的值有误！！！`
+        message: `${valueKey}的值有误！！！`,
       }
-      return
+      return;
     }
-    const sqlIns = `select * from logs ${condition} `
+    const sqlIns = `select * from logs ${condition} `;
     let res = await new Promise((resolve, reject) => {
-      db.query(sqlIns, function (err, results) {
+      db.query(
+        sqlIns, 
+        function (err, results) {
         if (err) {
-          reject({ status: 1, message: err.message })
+          reject({ status: 1, message: err.message });
         }
         else {
           // console.log(results);
-          let data = []
-          results.forEach(v => {
+          let data = [];
+          results.forEach( (v) => {
             let info = {
               ...v,
               pos: JSON.parse(v.pos),
               cpos: JSON.parse(v.cpos),
               img: JSON.parse(v.img),
-              video: JSON.parse(v.video)
+              video: JSON.parse(v.video),
             }
-            info.img.forEach(v=>{
-              v.url = config.server+v.url
+            info.img.forEach( (v) => {
+              v.url = config.server + v.url;
             })
-            info.video.forEach(v=>{
-              v.url = config.server+v.url
+            info.video.forEach( (v) => {
+              v.url = config.server + v.url;
             })
-            data.push(info)
+            data.push(info);
           })
-          resolve({ status: 0, message: '查询成功', data })
+          resolve({ status: 0, message: '查询成功', data });
         }
 
       })
     })
-    console.log(res.message)
-    ctx.body = res
+    console.log(res.message);
+    ctx.body = res;
   }
 
 }
